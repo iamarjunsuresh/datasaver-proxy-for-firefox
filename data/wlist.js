8473a332879a25afe290f2af437cd5ff68e80063
arjun suresh <123arjunsuresh@gmail.com>
2017-01-11 14:55:04 +0530

    version 2.1

---
+++ data/wlist.js
@@ -0,0 +1,23 @@
+/**
+ * Created by arjun on 29/6/16.
+ */
+
+
+
+var update=document.getElementById("btn_update");
+var  list=document.getElementById("list");
+
+update.addEventListener("click",function(){
+    self.port.emit("update-white-list",{list:list.value.split('\n')});
+    
+    
+})
+
+self.port.on("load", function onshow(data) {//console.log(data);
+
+    list.value=data.list.join('\n');
+    
+
+
+
+});
